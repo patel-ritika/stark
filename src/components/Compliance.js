11781dd29c5edf67f73bc3fed8c316ff814b6cf7
Your Name <you@example.com>
2024-11-18 19:25:57 +0530

    add(18/11/24)

--- src/components/Compliance.js
+++ src/components/Compliance.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ComplianceTop from './compliance/ComplianceTop';
+import ProductThird from './product/ProductThird';
+import DesignFour from './design/DesignFour';
+import DesignSecond from './design/DesignSecond';
+import DeveloperSecond from './developer/DeveloperSecond';
+import DesignFive from './design/DesignFive';
+import DesignFall from './design/DesignFall';
+import HomeFive from './home/HomeFive';
+import HomeThird from './home/HomeThird';
 
 const Compliance = () => {
   return (
     <div>
       <ComplianceTop/>
+      <ProductThird/>
+      <DesignFour/>
+      <DesignSecond/>
+      <DeveloperSecond/>
+      <DesignFive/>
+      <DesignFall/>
+      <HomeFive/>
+      <HomeThird/>
     </div>
   );
 }
