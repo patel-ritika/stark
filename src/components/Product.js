3413b54293e3d0d40bcdeb72a6e5e426ffd9c5f5
Your Name <you@example.com>
2024-11-18 18:42:51 +0530

    add(18/11/24)

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,6 +5,10 @@ import DesignFour from './design/DesignFour';
 import ProductThird from './product/ProductThird';
 import DesignSecond from './design/DesignSecond';
 import ProductFour from './product/ProductFour';
+import DesignFive from './design/DesignFive';
+import DesignFall from './design/DesignFall';
+import HomeFive from './home/HomeFive';
+import HomeThird from './home/HomeThird';
 
 const Product = () => {
   return (
@@ -15,6 +19,10 @@ const Product = () => {
       <ProductThird/>
       <DesignSecond/>
       <ProductFour/>
+      <DesignFive/>
+      <DesignFall/>
+      <HomeFive/>
+      <HomeThird/>
     </div>
   );
 }
