23df3fc94053c68b8e8fd9e7155a528f5637bb51
Your Name <you@example.com>
2024-11-18 19:18:12 +0530

    add(18/11/24)

--- src/components/compliance/ComplianceTop.js
+++ src/components/compliance/ComplianceTop.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import "../../asset/styles/compliance/compliancetop.css";
+import complianceImage from '../../asset/images/managerstop.jpeg';
+
 
 const ComplianceTop = () => {
   return (
-    <div>
-      hey
+    <div className="compliance-top">
+    <div className="compliance_top_content">
+      <h5>FOR COMPLIANCE MANAGERS</h5>
+      <h1>Centralize your<br /> accessibility<br />compliance</h1>
+      <h4>
+      Managing digital accessibility compliance requires the same<br />
+      level of rigor and prioritization as security and privacy. With<br />
+      Stark’s Compliance Center, managing your accessibility<br />
+      posture and growing your company’s accessibility maturity<br />
+      becomes centralized, actionable, automated.
+      </h4>
+      <div className="compliance_top_button-row">
+        <button className="compliance_top_btn-signup">Sign up for free</button>
+        <button className="compliance_top_btn-trial">Start a free trial</button>
+      </div>
     </div>
+    <div className="compliance_top_image-container">
+      <img src={complianceImage} alt="Design Illustration" />
+    </div>
+  </div>
   );
 }
 
